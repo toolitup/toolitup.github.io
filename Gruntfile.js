91a10e2de975f2fc16f54f7c46eb3a545ef37948
princejwesley <princejohnwesley@gmail.com>
2014-10-09 18:57:05 +0530

    Add Circular slider

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,53 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        mangle: true,
-        sourceMap : false,
-        banner: '/*! <%= pkg.name %> - V<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
-      },
-      dynamic_mappings: {
-        files: [{
-          expand: true,
-          cwd: 'private/js',
-          src: ['**/*.js'],
-          dest: 'public/assets/js/',
-          ext: '.min.js'
-        },],
-      },
-    },
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                mangle: true,
+                sourceMap: false,
+                banner: '/*! <%= pkg.name %> - V<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            dynamic_mappings: {
+                files: [{
+                    expand: true,
+                    cwd: 'private/js',
+                    src: ['**/*.js'],
+                    dest: 'public/assets/js/',
+                    ext: '.min.js'
+                }, ],
+            },
+        },
 
-   autoprefixer: {
+        autoprefixer: {
 
-      options: {
-        diff: true,
-        map: true,
-		browsers: ['> 1%', 'last 10 versions', 'Firefox ESR', 'Opera 12.1']
-      },
-      toolitup_css: {
-        src: 'public/assets/css/*.css'
-      },
-	  
-    },
-  });
+            options: {
+                diff: true,
+                map: true,
+                browsers: ['> 1%', 'last 10 versions', 'Firefox ESR', 'Opera 12.1']
+            },
+            toolitup_css: {
+                src: 'public/assets/css/*.css'
+            },
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  
-  // Default task(s).
-  grunt.registerTask('default', ['uglify', 'autoprefixer']);
+        },
+    });
 
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'autoprefixer']);
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint', 'uglify', 'autoprefixer']);
 };
---
+++ private/js/toolitup-circular-slider.js
@@ -0,0 +1,112 @@
+
+$(function() {
+	var fullCircularSlider = $('#slider-full').CircularSlider({ 
+		min : 0, 
+		max: 359, 
+		value : 10,
+		labelSuffix: "&deg",
+		slide : function(ui, value) {
+			ui.next().css({'background' : 'linear-gradient(' + value + 
+				'deg, white, cornsilk, white)'});
+		}
+	});	
+	
+	var halfCircularSlider = $('#slider-half-circle').CircularSlider({ 
+		min : 0, 
+		max: 100, 
+		value : 60,
+		shape: "Half Circle",
+		labelSuffix: "%",
+		slide : function(ui, value) {
+			if (value < 50) {
+				ui.next().css({'background' : 'rgba(250, 81, 81,' + ((100 - value) / 100) + ')'});
+			} else {
+				ui.next().css({'background' : 'rgba(87, 250, 87,' + (value / 100) + ')'});			
+			}
+		}
+	});	
+	
+
+	var halfRightCircularSlider = $('#slider-half-circle-right').CircularSlider({ 
+		min : 0, 
+		max: 100, 
+		value : 40,
+		shape: "Half Circle Right",
+		labelSuffix: " kph",
+		slide : function(ui, value) {
+			var alpha = value / 100;
+			if (value <= 50) {
+				ui.next().css({'background' : 'rgba(87, 250, 87,' + alpha + ')'});			
+			}
+			else if (value <= 80) {
+				ui.next().css({'background' : 'rgba(255, 235, 0,' + alpha + ')'});			
+			} else {
+				ui.next().css({'background' : 'rgba(250, 81, 81,' + alpha + ')'});
+			}
+		}
+	});	
+
+	var halfLeftCircularSlider = $('#slider-half-circle-left').CircularSlider({ 
+		min : 0, 
+		max: 100, 
+		value : 40,
+		shape: "Half Circle Left",
+		labelSuffix: " kph",
+		slide : function(ui, value) {
+			var alpha = value / 100;
+			if (value <= 50) {
+				ui.next().css({'background' : 'rgba(87, 250, 87,' + alpha + ')'});			
+			}
+			else if (value <= 80) {
+				ui.next().css({'background' : 'rgba(255, 235, 0,' + alpha + ')'});			
+			} else {
+				ui.next().css({'background' : 'rgba(250, 81, 81,' + alpha + ')'});
+			}
+		}
+	});	
+	
+	var halfBottomCircularSlider = $('#slider-half-circle-bottom').CircularSlider({ 
+		min : -50, 
+		max: 50, 
+		value : 20,
+		shape: "Half Circle Bottom",
+		labelPrefix: "&#9661;",
+		slide : function(ui, value) {
+			var alpha = Math.abs(value) / 100;
+			ui.next().css({'background' : 'rgba(7, 154, 252,' + alpha + ')'});			
+		}
+	});	
+	
+	var imageSlider = $('#slider-customized').CircularSlider({ 
+		min : 0, 
+		max: 359,
+		radius: 100,
+		innerCircleRatio : .7,
+		formLabel : function(value, prefix, suffix) {
+			return '<img src="assets/images/baby'+ parseInt(value / 26)+'.png"></img>';
+		},
+		slide : function(ui, value) {
+			var colors = ['deeppink', 'seagreen', 'deepskyblue', 'coral', 'cadetblue', 'olive', 'chocolate',
+				'yellowgreen', 'cornflowerblue', 'slategrey', 'salmon', 'brown', 'darkgoldenrod', 'dimgrey'];
+			var color = colors[parseInt(value / 26)];
+			ui.find('.jcs').css({'border-color' : color });
+			ui.find('.jcs-indicator').css({'background' : color });
+		}
+	});	
+
+	
+	$("div[class^='cs-demo-'").css('display', 'none');
+	
+	$('.cs-demo-1').css('display', 'table');
+	
+	$('#menu').children().on('click', function(e) {
+	
+		var demo = $(this).data().menuanchor;
+
+		$('div[class^="cs-demo-"').css('display', 'none');
+		$('.' + demo).css('display', 'table');		
+		
+	});
+	
+});
+
