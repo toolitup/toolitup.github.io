295c54a20e4373b9e08ee95a8f266dfcd1bce185
Senthil Porunan <senthilraja39@gmail.com>
2015-04-14 15:35:36 +0530

    :bug: bug

--- private/js/toolitup-jrate.js
+++ private/js/toolitup-jrate.js
@@ -77,7 +77,7 @@ $(function() {
 	generateJRate('demo-opacity', {opacity: 0.3});
 
 	generateJRate('demo-min-max', {min: 10, max: 15, rating: 11});
-	generateJRate('demo-min-selected', {minSelected: 1});
+	generateJRate('demo-min-selected', {minSelected: 3});
 	generateJRate('demo-precision', {precision: 0.5});
 	generateJRate('demo-horizontal', {horizontal: false});
 	generateJRate('demo-reverse', {reverse: true});
